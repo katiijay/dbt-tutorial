cacdf383fb02e585cfed68fcd87a1824eb7c7038
katiijay <katherine.pillsbury@gmail.com>
2023-02-24 16:51:50 -0800

    commit references

--- models/example/most_used_skills.sql
+++ models/example/most_used_skills.sql
@@ -7,10 +7,10 @@
 SELECT 
 skill_name
 ,SUM(Duration_Months*usage_level) AS usage_months_aggregation
-FROM resume_dev.skills sk
-LEFT JOIN resume_dev.job_skill_map jsm
+FROM {{ref('skills')}} sk
+LEFT JOIN {{ref('job_skill_map')}} jsm
 	ON jsm.skill_id = sk.Skill_ID
-LEFT JOIN resume_dev.job_history jh
+LEFT JOIN {{ref('job_history')}} jh
 	ON jh.Job_ID = jsm.job_id
 GROUP BY skill_name
 ORDER BY usage_months_aggregation DESC
